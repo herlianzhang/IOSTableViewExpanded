5f9bc24ac9a0804d7e5e20eb9047dac46f11114e
herlianzhang <herlianzhang370@gmail.com>
2021-08-09 10:18:50 +0700

    Initial Commit

---
+++ IOSExpandedTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSExpandedTableView
+//
+//  Created by Ripin Li on 09/08/21.
+//  Copyright © 2021 Herlian Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
